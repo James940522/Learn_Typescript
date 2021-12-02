84705776185f57432680a4b497304304e4be51ba
james940522 <James940522@gmail.com>
2021-12-02 23:18:25 +0900

    첫 번째 장점

--- why-ts/app.js
+++ why-ts/app.js
@@ -9,12 +9,46 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+function fetchUser() {
+  return axios.get(url)
+}
+
+/**
+ * @typedef {object} Adress
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Adress} address
+ */
+
+
+/**
+ *  @returns {Promise<User>}
+ */
+
+fetchUser().then((response)=> {
+  response.address.city
+})
+
+
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
+      console.log(user)
+      username.innerText = user[0].name;
+      email.innerText = user[0].email;
+      address.innerText = user[0].address.street;
       // TODO: 이름, 이메일, 주소 표시하기
     })
     .catch(function (error) {
