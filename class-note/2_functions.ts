d56445acd15b53fd0851327ee8fdec680fca4839
james940522 <James940522@gmail.com>
2021-12-06 20:47:03 +0900

    함수 타입정의-2

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -9,6 +9,15 @@ function add(): number {
     return 10;
 }
 
+// 함수의 타입을 정의하는 방식
 function sum(a: number, b: number): number {
     return a + b;
 }
+sum(10, 20); // 30
+// 함수를 정의할때 인수를 2개로 받기로 하였으니, 이 보다 많거나 적으면 에러가 발생
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {
+
+}
+// 특정 파라미터를 선택적으로 사용하고싶을때에는 ?를 사용한다.
