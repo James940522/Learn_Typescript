734aada54d9b0fce8fe33a9daf8f6b6d71121064
james940522 <James940522@gmail.com>
2021-12-12 20:30:54 +0900

    타입 별칭 코드 예제

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,14 @@
+// interface PersonA {
+//     name: string;
+//     age: number;
+// }
+
+type PersonA = {
+    name: string;
+    age: number;
+}
+
+let seh: PersonA = {
+    name: '세호',
+    age: 30
+}
