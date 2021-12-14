4fdb09c474695507dad574828009dd1d7cc7f820
james940522 <James940522@gmail.com>
2021-12-14 22:35:31 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -3,11 +3,21 @@
 // }
 
 // Union Type
-
-function logMessage(value: string | number | boolean) {
-    console.log(value);
+let seho : string | number | boolean;
+function logMessage(value: string | number) {
+    if (typeof value === 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value === 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number')
 }
+
+// value를 any로 해도 if절 안에서 만약 number일때를 가정한다면 그 스코프에서는 value가 number가 된다.
+// 타입가드: 특정 타입으로 타입의 범위를 좁혀나가는(필터링 하는) 과정
 
 logMessage('hello');
 logMessage(100);
 logMessage(false);
+// logMessage(['FDSF'])
