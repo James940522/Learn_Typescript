94c982f4874d823900bb892ed83e930426478d9e
james940522 <James940522@gmail.com>
2021-12-19 22:49:01 +0900

    클래스 복습

---
+++ class-note/class.js
@@ -0,0 +1,14 @@
+// ES2015 (ES6)
+
+class Person {
+    //클래스 로직
+    constructor(name, age) {
+        console.log('생성 되었습니다.')
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const jm = new Person('재민', 28); // 생성 되었습니다,
+
+console.log(jm)
