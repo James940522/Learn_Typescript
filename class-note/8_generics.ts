c20ee99c138aefe464e94826ff8e21213ad90fc9
Jaemin Jung <james940522@gmail.com>
2022-02-01 18:19:58 +0900

    keyof로 타입 제한하기

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -26,6 +26,7 @@ const obj: Dropdown<string> = { value: "하이", selected: true };
 
 // logTextLength<string>(["hi", "hello"]);
 
+//제네릭의 정의된 타입 제한
 interface LengthType {
   length: number;
 }
@@ -36,3 +37,18 @@ function logTextLength<T extends LengthType>(text: T): T {
 }
 logTextLength("a");
 logTextLength({ value: "가나다라마바사아자차", length: 10 });
+
+// keyof 타입 제한
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+// ShoppingItem의 타입중 하나를 타입으로 갖는다
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>("a");
+getShoppingItemOption("name");
